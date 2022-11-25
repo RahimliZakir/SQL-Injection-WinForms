b8ad30fc0c36553e9391c55aa57140395bd0f64f
RahimliZakir <zakirer@code.edu.az>
2022-11-25 17:34:01 +0400

    INSERT Statements Written

---
+++ Project.Solution/Project.WindowsFormsApp/Form.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Project.WindowsFormsApp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.surnameLabel = new System.Windows.Forms.Label();
+            this.surnameTextBox = new System.Windows.Forms.TextBox();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(13, 63);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(289, 22);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 44);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(44, 16);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // surnameLabel
+            // 
+            this.surnameLabel.AutoSize = true;
+            this.surnameLabel.Location = new System.Drawing.Point(12, 109);
+            this.surnameLabel.Name = "surnameLabel";
+            this.surnameLabel.Size = new System.Drawing.Size(61, 16);
+            this.surnameLabel.TabIndex = 3;
+            this.surnameLabel.Text = "Surname";
+            // 
+            // surnameTextBox
+            // 
+            this.surnameTextBox.Location = new System.Drawing.Point(13, 128);
+            this.surnameTextBox.Name = "surnameTextBox";
+            this.surnameTextBox.Size = new System.Drawing.Size(289, 22);
+            this.surnameTextBox.TabIndex = 2;
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(12, 171);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(32, 16);
+            this.ageLabel.TabIndex = 5;
+            this.ageLabel.Text = "Age";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(13, 190);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(289, 22);
+            this.ageTextBox.TabIndex = 4;
+            // 
+            // button
+            // 
+            this.button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button.Location = new System.Drawing.Point(15, 248);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(287, 36);
+            this.button.TabIndex = 6;
+            this.button.Text = "Add";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 314);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.surnameLabel);
+            this.Controls.Add(this.surnameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label surnameLabel;
+        private System.Windows.Forms.TextBox surnameTextBox;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Project.Solution/Project.WindowsFormsApp/Form.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Project.WindowsFormsApp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        const string cString = "Data Source=.\\SQLEXPRESS;Initial Catalog=SQL-Injection-DB;User ID=sa;password=query;MultipleActiveResultSets=True;";
+        private void button_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cString);
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[Person]([Name], [Surname], [Age])
+                                                      VALUES(@name, @surname, @age)", con);
+
+            con.Open();
+
+            cmd.Parameters.AddWithValue("@name", nameTextBox.Text);
+            cmd.Parameters.AddWithValue("@surname", surnameTextBox.Text);
+            cmd.Parameters.AddWithValue("@age", ageTextBox.Text);
+
+            cmd.ExecuteNonQuery();
+
+            cmd.Dispose();
+            con.Close();
+
+            MessageBox.Show("Yeni məlumat əlavə olundu!", "Uğurlu!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
