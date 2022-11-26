11006e15083fd2b772a0d500916e6f977c0044d1
RahimliZakir <zakirer@code.edu.az>
2022-11-26 12:11:28 +0400

    SQL Example Added!

---
+++ Project.Solution/Project.WindowsFormsApp/SQL Examples/InjectionExampleSQLQuery.sql
@@ -0,0 +1,10 @@
+INSERT INTO [dbo].[Person]([Name], [Surname], [Age])
+VALUES('Zakir', 'Rahimli', 20) TRUNCATE TABLE [dbo].[Person]--', 20)
+
+-- Injection Value -> Rahimli', 20) TRUNCATE TABLE [dbo].[Person]--
+
+-- This is Injection!
+
+RETURN
+
+SELECT * FROM [dbo].[Person]
